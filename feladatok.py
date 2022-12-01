b6d17a30b00798533829837e9e9c830814d92dba
vizsga28 <vizsga28@vasvari.hu>
2022-12-01 10:30:19 +0100

    kész feladatok

--- feladatok.py
+++ feladatok.py
@@ -6,19 +6,48 @@
 # A másik bemenő paraméter egy keresett megyeszékhely neve.
 # A függvény határozza meg, hogy a keresett megyeszékhely szerepel-e a listában!
 
-def megtalalhato(megyeszekhelyek:list, keresett) -> bool:
-    return False
+def megtalalhato(megyeszekhelyek:list, keresett:str) -> bool:
+    if len(megyeszekhelyek) == 0:
+        return False
 
+    for i in megyeszekhelyek:
+        if i == keresett:
+            return True
+    return False
 # 2. feladat
 # Készítsen függvényt jeles néven!
 # A függvény bemenete egy jegyekből álló karakterlánc: "4, 2, 4, 3, 2, 3, 1"!
 # A függvény számolja meg, hogy hány tantárgyból ért el jeles (5) eredményt a diák!
+def jeles(jegy:str) -> int:
+    db = 0
+    seged = jegy.split(", ")
+    for i in seged:
+        if i == "5":
+            db += 1
+    return db
 
 # 3. feladat
 # Készítsen függfényt minhely néven!
 # A függvény bemenete egy számokból álló karakterlánc: 2,3; 5,4; 2,0; 1,9; 4,22; 3,7!
 # Határozza meg a legkisebb szám sorszámát (az első számot egytől sorszámozzuk)!
 # Ha nincsenek számok térjen vissza None értékkel!
+
+def minhely(szamok:str) -> int:
+    seged = szamok.replace(',','.')
+    seged2 = seged.split("; ")
+    min = seged2[0]
+    db = 1
+    if szamok == "":
+        return None
+    for i in seged2:
+        if i < min:
+            min = i
+    for j in seged2:
+        if j != min:
+            db += 1
+        else:
+            break
+    return db
 
 # 4. feladat
 # A feladat csak akkor csinálja meg, ha minden feladattal végzett!
